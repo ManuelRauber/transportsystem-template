05b89e40379b86b2b1a9b387448f245ebc49680d
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-11-16 11:27:44 +0100

    Code

--- src/app/websocket/websocket.component.ts
+++ src/app/websocket/websocket.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './websocket.component.html',
   styleUrls: ['./websocket.component.css']
 })
-export class WebsocketComponent implements OnInit {
+export class WebsocketComponent {
 
+  private _ws: WebSocket;
+
+  public command: string;
+
+  public history: Array<string> = [];
+
+  // TODO: In einen Angular 2 Service schieben
   constructor() { }
 
-  ngOnInit() {
+  public connect() {
+    this._ws = new WebSocket('ws://192.168.0.1:8080');
+
+    this._ws.onmessage = event => {
+      this.history.push('[SERVER] ' + event.data);
+    };
   }
 
+  public send() {
+    if (!this.command) {
+      return;
+    }
+
+    this.history.push('[CLIENT] ' + this.command);
+
+    this._ws.send(this.command);
+
+    this.command = '';
+  }
 }
